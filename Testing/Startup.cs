ecefe2b174d9ab4a3ba9db80f8bf98417a7bcb2e
amanda-delongchamp <amanda.delongchamp@gmail.com>
2021-08-18 09:54:39 -0700

    Adds Product View and DB Connection

--- Testing/Startup.cs
+++ Testing/Startup.cs
@@ -1,7 +1,9 @@
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
+
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -24,6 +26,8 @@ namespace Testing
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddDbContext<IDbConnection>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("bestbuy")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
